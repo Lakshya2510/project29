0c7418f4b48c23db790fa06436ceba1350cad290
Lakshya2510 <74653849+Lakshya2510@users.noreply.github.com>
2021-02-01 09:57:02 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.3,
+            'friction':1.0,
+            'density':2
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER)
+        fill("lightBlue")
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+}
